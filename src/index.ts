cbf28d296bade6868317a5942300514babf19db9
serialbandicoot <samtreweek@gmail.com>
2024-09-19 15:53:55 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { test, Locator } from '@playwright/test';
-// @ts-expect-error - no types
-import jestExpect from 'expect/build/matchers';
 import { ImageClassificationMetadata, validateMetadata } from './metadata';
 
 import {
@@ -16,18 +14,12 @@ import {
 import { createLocatorImage } from './image';
 
 const PlaywrightClassification = {
-  async toImageClassification(
-    locator: Locator,
-    expected: string,
-    options?: { threshold?: number; model?: string },
-  ) {
-
+  async toImageClassification(locator: Locator, expected: string, options?: { threshold?: number; model?: string }) {
     // Check if 'expected' is an empty string
-    if (!expected || expected.trim() === "") {
-      throw new Error("Expected classification cannot be an empty string");
+    if (!expected || expected.trim() === '') {
+      throw new Error('Expected classification cannot be an empty string');
     }
 
-    const expectedMatcherName = 'toImageClassification';
     // Get metadata from playright.config.ts
     const metadata = test.info().config.metadata;
     const info = test.info();
@@ -58,9 +50,9 @@ const PlaywrightClassification = {
       throw new Error(`There is no label '${expected}' found in '${metaLabels.join(', ')}'`);
     }
 
-    let threhold = 0.95;
+    let threshold = 0.95;
     if (options?.threshold) {
-      threhold = options?.threshold;
+      threshold = options?.threshold;
     }
 
     // Generate Screenshot from Locator
@@ -89,9 +81,9 @@ const PlaywrightClassification = {
       // Binary || Category
       const type = getClassificationType(model);
       if (type == ClassificationType.Binary) {
-        actual = mapBinaryPrediction(predictions, threhold, imageModel.image.labels);
+        actual = mapBinaryPrediction(predictions, threshold, imageModel.image.labels);
       } else if (type == ClassificationType.Category) {
-        actual = mapCategoryPrediction(predictions, threhold, imageModel.image.labels);
+        actual = mapCategoryPrediction(predictions, threshold, imageModel.image.labels);
       } else {
         throw new Error('No Classification Type found');
       }
@@ -99,13 +91,14 @@ const PlaywrightClassification = {
 
     // Evaluate the scores!
     if (actual.highestLabel !== expected) {
-      const scoreMessage = actual.highestValue 
-        ? ` with a prediction score of: ${parseFloat(actual.highestValue.toFixed(2))}` 
+      const scoreMessage = actual.highestValue
+        ? ` with a prediction score of: ${parseFloat(actual.highestValue.toFixed(2))}`
         : '';
 
       return {
         pass: false,
-        message: () => `Expected Label to classify as '${expected}', but the highest classification was '${actual.highestLabel}'${scoreMessage}`,
+        message: () =>
+          `Expected Label to classify as '${expected}', but the highest classification was '${actual.highestLabel}'${scoreMessage}`,
       };
     }
 
@@ -114,7 +107,6 @@ const PlaywrightClassification = {
       pass: true,
       message: () => 'Passed',
     };
-
   },
 };
 
